c4b3de0869e5fa5a1c48361fc89e04847e9ae639
Kseniia <xeniiatikhomirova@gmail.com>
2024-02-21 20:37:21 +0400

    optimised the behavior during tha game -> stopped alse the option to shoot. Changes in chechShoots.js

--- checkShoots.js
+++ checkShoots.js
@@ -1,15 +1,19 @@
+let shoot;
+
 function checkShoots() {
-   if (KEY_S) {
-      let shoot = {
-         x: rocket.x + rocket.width,
-         y: rocket.y + (rocket.width / 4.5),
-         width: 60,
-         height: 40,
-         src: 'img/shell.png', 
-         img: new Image()
-      };
-      shoot.img.src = shoot.src;
-      shoots.push(shoot);
+   if(gameStatus){
+      if (KEY_S) {
+         shoot = {
+            x: rocket.x + rocket.width,
+            y: rocket.y + (rocket.width / 4.5),
+            width: 60,
+            height: 40,
+            src: 'img/shell.png', 
+            img: new Image()
+         };
+         shoot.img.src = shoot.src;
+         shoots.push(shoot);
+      }
    }
 };
 
